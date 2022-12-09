27a07fce277bd7e05c78f239e3fe08a4d0643670
ferdy <ferdy.lim@springkraf.com>
2022-12-09 18:48:52 +0700

    feat: initialize project, user reducer

---
+++ src/states/authUser/action.js
@@ -0,0 +1,48 @@
+import api from "../../utils/api";
+
+/**
+ * @TODO: Define all the actions (creator) for the authUser state
+ */
+export const ActionType = {
+  SET_AUTH_USER: "SET_AUTH_USER",
+  UNSET_AUTH_USER: "UNSET_AUTH_USER",
+};
+
+export function setAuthUserActionCreator(authUser) {
+  return {
+    type: ActionType.SET_AUTH_USER,
+    payload: {
+      authUser,
+    },
+  };
+}
+
+export function unsetAuthUserActionCreator() {
+  return {
+    type: ActionType.UNSET_AUTH_USER,
+    payload: {
+      authUser: null,
+    },
+  };
+}
+
+export function asyncSetAuthUser({ id, password }) {
+  return async (dispatch) => {
+    try {
+      const token = await api.login({ id, password });
+      api.putAccessToken(token);
+      const authUser = await api.getOwnProfile();
+
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export function asyncUnsetAuthUser() {
+  return (dispatch) => {
+    dispatch(unsetAuthUserActionCreator());
+    api.putAccessToken("");
+  };
+}
