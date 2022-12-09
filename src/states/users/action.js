e8e2a2fa5183582e217963a46613868663381388
ferdy <ferdy.lim@springkraf.com>
2022-12-09 19:01:11 +0700

    feat:users reducer

--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,3 +1,27 @@
+import api from "../../utils/api";
+
 /**
  * @TODO: Define all the actions (creator) for the users state
  */
+export const ActionType = {
+  RECEIVE_USERS: "RECEIVE_USERS",
+};
+
+export function receiveUsersActionCreator(users) {
+  return {
+    type: ActionType.RECEIVE_USERS,
+    payload: {
+      users,
+    },
+  };
+}
+
+export function asyncRegisterUser({ id, name, password }) {
+  return async () => {
+    try {
+      await api.register({ id, name, password });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
